7901324c6502cdaba8ebc92ed84ecbd1c7395191
Nitin <nitiniiitr@gmail.com>
2025-11-23 10:51:35 +0530

    bcrypt added and salt and hash

--- models/Person.js
+++ models/Person.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 // defining person schema
+const bcrypt =require('bcrypt')
 const personSchema= new mongoose.Schema({
     name:{
         type:String,
@@ -39,5 +40,37 @@ const personSchema= new mongoose.Schema({
     }
 });
 // createw person model
+personSchema.pre('save',async function (next) {
+const person = this
+// this reperesents jb bhi hm save krna chah rhenge honger to premiddkleware perform hoga
+
+// hash the password only if it is modified (or is new)
+
+if(!person.isModified('password')) return next()
+    try {
+        // hash password generation
+const salt = await bcrypt.genSalt(10)
+// hash password
+const hashedpassword = await bcrypt.hash(person.password,salt)
+// overide plain password with hashed one 
+person.password = hashedpassword
+      next()
+} catch (err) {
+   return next(err) 
+}
+})
+personSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        const isMatch = await bcrypt.compare(candidatePassword,this.password)
+        return isMatch  
+    } catch (err) {
+        throw err
+    }
+}
+// prince -- > bdbebuebjebe7y38eh3jbeu3heb3
+// login --- > agarwak
+
+// bdbebuebjebe7y38eh3jbeu3heb3 -- > extract salt
+// salt+agarwak -- > hash -- > svwsnwkmskwmswbshwvdhwjdnbe
 const Person = mongoose.model('Person',personSchema);
 module.exports=Person;
